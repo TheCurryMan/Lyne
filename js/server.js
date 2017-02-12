b982fb8a2ddb4903985359ff92e8216350894620
Avinash Jain <avinashkalpitjain@gmail.com>
2017-02-12 00:42:27 -0800

    Updated Code

--- js/server.js
+++ js/server.js
@@ -22,7 +22,7 @@ function createCORSRequest(method, url) {
 // Make the actual CORS request.
 function makeCorsRequest(message, number) {
     // This is a sample server that supports CORS.
-    var url = 'https://shielded-reef-48843.herokuapp.com/?='+message+'&phone='+number;
+    var url = 'https://shielded-reef-48843.herokuapp.com/?message='+message+'&phone='+number;
 
     var xhr = createCORSRequest('GET', url);
     if (!xhr) {
