18370dcaa4740dc5922982caabee3192b5f034db
rohandkn <rohandhesikan@gmail.com>
2017-02-12 05:03:04 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -142,6 +142,8 @@ function notHere() {
 }
 
 function next() {
+    stop();
+    reset();
     counter+=1;
     getTime();
     if(items.length>1)
@@ -250,4 +252,45 @@ ctx.stroke();
 
 }
   }
+var h1 = document.getElementsByTagName('h1')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
+
+
+/* Start button */
+//start.onclick = timer;
+
+/* Stop button */
+function stop() {
+    clearTimeout(t);
+}
+
+/* Clear button */
+function reset() {
+    h1.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
 init(); 
